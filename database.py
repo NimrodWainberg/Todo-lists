12291657f40c2af032eeb4003ac149a658c24ee3
Nimrod <n.wainberg@gmail.com>
2022-04-23 02:07:38 +0300

    [fix]- MongoDB url

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from model import Todo
 import motor.motor_asyncio
 
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb//localhost:27017')
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
 
 # Database TodoList
 database = client.TodoList
