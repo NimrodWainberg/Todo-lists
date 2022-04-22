1a3e529b3fb1ca0c25934cd37592f5d6cf1021b8
Nimrod <n.wainberg@gmail.com>
2022-04-23 02:23:00 +0300

    [Done]- RestApi

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from model import Todo
 
+from database import(
+    fetch_all_todo,
+    fetch_one_todo,
+    create_todo,
+    update_todo,
+    remove_todo,
+)
 # App
 app = FastAPI()
 
@@ -8,7 +16,7 @@ app = FastAPI()
 # Allows react to connect to the backend
 origins = ['https://localhost:3000']
 
-#
+# Middleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -16,3 +24,46 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.get("/")
+def read_route():
+    return {"Ping": "Pong"}
+
+
+@app.get("/api/todo")
+async def get_todo():
+    response = await fetch_all_todo()
+    return response
+
+
+@app.get("/api/todo{title}", response_model=Todo)
+async def get_todo_by_id(title):
+    response = await fetch_one_todo(title)
+    if response:
+        return response
+    raise HTTPException(404, f"Could not find a todo with this title {title}")
+
+
+@app.post("/api/todo", response_model=Todo)
+async def post_todo(todo: Todo):
+    response = await create_todo(todo.dict())
+    if response:
+        return response
+    raise HTTPException(400, f"Something went wrong")
+
+
+@app.put("/api/todo{title}", response_model=Todo)
+async def put_todo(title: str, description: str):
+    response = await update_todo(title, description)
+    if response:
+        return response
+    raise HTTPException(404, f"Could not find a todo with this title {title}")
+
+
+@app.delete("/api/todo{title}")
+async def delete_todo(title):
+    response = await remove_todo(title)
+    if response:
+        return response
+    raise HTTPException(404, f"Could not find a todo with this title {title}")
