fe27b93802ebc3be8605eb1bc4d0840ff9cf0064
Nimrod <n.wainberg@gmail.com>
2022-06-06 03:55:02 +0300

    test- Add more tests

--- frontend/src/__tests__/task.test.js
+++ frontend/src/__tests__/task.test.js
@@ -1,8 +1,29 @@
-import { render, screen, clean } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
+import renderer from "react-test-renderer";
 import Task from "../components/task";
+
+afterEach(() => {
+  cleanup();
+});
 
 test("should render task component", () => {
   render(<Task />);
   const taskElement = screen.getByTestId("task");
   expect(taskElement).toBeInTheDocument();
+});
+
+test("should render good task component", () => {
+  const task = { title: "unique", description: "test" };
+  render(<Task task={task} />);
+  const taskElement = screen.getByTestId("task");
+  expect(taskElement).toBeInTheDocument();
+  expect(taskElement).toHaveTextContent(task.title);
+  // expect(taskElement).toContainHtml("<div>");
+  // expect(taskElement).not.toContainHtml("<div>");
+});
+
+test("matches snapshot", () => {
+  const task = { title: "unique", description: "test" };
+  const tree = renderer.create(<Task task={task} />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
--- frontend/src/__tests__/tasks.test.js
+++ frontend/src/__tests__/tasks.test.js
@@ -1,9 +1,19 @@
-import { render, screen, clean } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
+import renderer from "react-test-renderer";
 import Tasks from "../components/tasks";
 
-test("should render taks component", () => {
+afterEach(() => {
+  cleanup();
+});
+
+test("should render tasks component", () => {
   render(<Tasks />);
   const taskElement = screen.getByTestId("tasks");
   expect(taskElement).toBeInTheDocument();
   expect(taskElement).toHaveTextContent("Tasks List");
+});
+
+test("matches snapshot", () => {
+  const tree = renderer.create().toJSON();
+  expect(tree).toMatchSnapshot();
 });
